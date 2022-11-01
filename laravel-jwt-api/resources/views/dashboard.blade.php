c31ad74a3c21d58580120c0b8b85864b8b9d233b
Paul <ing12xunder@gmail.com>
2022-10-31 21:26:06 -0500

    Actividad
    PHP

---
+++ laravel-jwt-api/resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Login del usuario correcto
+                    </div> 
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm">
+                    <ul>
+                    @foreach ($users as $user)
+                        {{ $user->name }}
+                    @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+</html>
